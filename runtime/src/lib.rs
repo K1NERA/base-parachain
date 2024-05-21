9e9627e2a239b6eaa7603739aac613ecadbb068a
antonioarieiro <sfgfffs@gmail.com>
2024-05-21 10:22:45 -0300

    fix: build target

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,8 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
-// Make the WASM binary available.
+
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
@@ -128,10 +127,7 @@ impl WeightToFeePolynomial for WeightToFee {
 	}
 }
 
-/// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
-/// the specifics of the runtime. They can then be made to be agnostic over specific formats
-/// of data like extrinsics, allowing for them to continue syncing the network through upgrades
-/// to even the core data structures.
+
 pub mod opaque {
 	use super::*;
 	use sp_runtime::{
